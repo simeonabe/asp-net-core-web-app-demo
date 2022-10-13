e8dc24fb71411973b7a40d95cb7f83d047b527a7
Simeon Abraham <43566899+simeonabe@users.noreply.github.com>
2022-10-13 03:54:28 -0800

    Add files via upload

---
+++ asp-net-core-web-app/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace asp_net_core_web_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
